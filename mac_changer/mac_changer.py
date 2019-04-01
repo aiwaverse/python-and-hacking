5069110b1d17c4d5609425c75de798b1b8e7c6c2
aiwaverse <aiwavision@protonmail.com>
2019-03-31 22:12:15 -0300

    changes to the changer of mac adress

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 import subprocess
-subprocess.call("ifconfig", shell=True)
+interface = "enp3s0"
+new_mac = "00:11:22:33:44:56"
+print("Changing MAC adress for " + interface + " to: " + new_mac)
+subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+subprocess.call("sudo ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("sudo ifconfig " + interface + " up", shell=True)
 
