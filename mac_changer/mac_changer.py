30c7ef5fb2e27c628b55308a57ce95ba2bbd86f7
aiwaverse <aiwavision@protonmail.com>
2019-04-28 13:37:42 -0400

    now using functions

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = input("Interface to change the MAC adress > ")
-new_address = input("New MAC address > ")
-print("[+] Changing MAC address for " + interface + " to " + new_address)
-# old calls, insecure
-# subprocess.call("sudo ifconfig " + interface + " down", shell=True)
-# subprocess.call("sudo ifconfig " + interface + " hw ether " + new_address, shell=True)
-# subprocess.call("sudo ifconfig " + interface + " up", shell=True)
-subprocess.call(["sudo", "ifconfig", interface, "down"])
-subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_address])
-subprocess.call(["sudo", "ifconfig", interface, "up"])
+def change_the_mac_address (interface, mac_address):
+    print("[+] Changing MAC address for " + interface + " to " + mac_address)
+    subprocess.call(["sudo", "ifconfig", interface, "down"])
+    subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", mac_address])
+    subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's MAC Address")
+parser.add_option("-m", "--mac", dest="new_address", help="New MAC Address for the interface")
+(options, arguments) = parser.parse_args()
+change_the_mac_address(options.interface, options.new_address)
