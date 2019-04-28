c4b7da3cbbe6dfe457f7331c8dced3d18dd822e8
aiwaverse <aiwavision@protonmail.com>
2019-04-28 13:04:42 -0400

    Updated the code to be more secure

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -2,9 +2,13 @@
 
 import subprocess
 
-interface = input("New interface > ")
+interface = input("Interface to change the MAC adress > ")
 new_address = input("New MAC address > ")
 print("[+] Changing MAC address for " + interface + " to " + new_address)
-subprocess.call("sudo ifconfig " + interface + " down", shell=True)
-subprocess.call("sudo ifconfig " + interface + " hw ether " + new_address, shell=True)
-subprocess.call("sudo ifconfig " + interface + " up", shell=True)
+# old calls, insecure
+# subprocess.call("sudo ifconfig " + interface + " down", shell=True)
+# subprocess.call("sudo ifconfig " + interface + " hw ether " + new_address, shell=True)
+# subprocess.call("sudo ifconfig " + interface + " up", shell=True)
+subprocess.call(["sudo", "ifconfig", interface, "down"])
+subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_address])
+subprocess.call(["sudo", "ifconfig", interface, "up"])
